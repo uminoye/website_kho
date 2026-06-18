7b17da7a4f7712c4460d5ffae76f132e8e45400a
uminoye <minhthipham857@gmail.com>
2026-06-18 11:51:20 +0700

    Don dep node_modules va sua loi push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/website_kho/',
+  root: path.resolve(__dirname, '..'),
 })
